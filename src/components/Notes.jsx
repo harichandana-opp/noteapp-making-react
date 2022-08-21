ec9b288d6a67efae7cd68bd6b0490bc19b27f062
harichandana-opp <92313264+harichandana-opp@users.noreply.github.com>
2022-08-21 21:42:47 +0530

    Initial commit

---
+++ src/components/Notes.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Notes() {
+  return (
+    <div className="note">
+      <h1>React Project</h1>
+      <p>
+        This my first react project so excited to do this and thanks for shaurya
+        sinha sir for teaching all these stuffs
+      </p>
+    </div>
+  );
+}
+export default Notes;
